5b0c4f14cb6c79bfd4af13438811cea957b89b2b
github username <github email address>
2018-02-21 16:39:46 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,9 @@
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara/rspec'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require './app'
